d94f1eaa43c1b64121822bd0f60ffabd5f53ce18
Jernej Virag <jernej@virag.si>
2015-09-16 21:07:37 +0200

    Fix possible play services crash

--- promet/src/main/java/si/virag/promet/map/LocationModule.java
+++ promet/src/main/java/si/virag/promet/map/LocationModule.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -44,8 +45,10 @@ public class LocationModule implements GoogleApiClient.ConnectionCallbacks, Loca
     }
 
     public synchronized Location getLocationWithTimeout(int timeoutMs) {
-        if (!googleApiClient.isConnected())
-            googleApiClient.blockingConnect(timeoutMs, TimeUnit.MILLISECONDS);
+        if (!googleApiClient.isConnected()) {
+            ConnectionResult result = googleApiClient.blockingConnect(timeoutMs, TimeUnit.MILLISECONDS);
+            if (!result.isSuccess()) return null;
+        }
 
         long currentTime = Calendar.getInstance().getTimeInMillis();
         if (currentLocation != null && timeDifference(currentLocation.getTime(), currentTime) < CURRENT_LOCATION_MAX_AGE_MS) {
