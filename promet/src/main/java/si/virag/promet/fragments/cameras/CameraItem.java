519489b25a8c9a4e598bfbc12771aeb685cc6cd6
Jernej Virag <jernej@virag.si>
2016-07-16 17:57:40 +0200

    Tweak map display to make it usable

--- promet/src/main/java/si/virag/promet/fragments/cameras/CameraItem.java
+++ promet/src/main/java/si/virag/promet/fragments/cameras/CameraItem.java
@@ -1,12 +1,19 @@
 package si.virag.promet.fragments.cameras;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
 
@@ -45,27 +52,73 @@ public class CameraItem extends AbstractSectionableItem<CameraItem.CameraItemHol
     @SuppressLint("SetTextI18n")
     @Override
     public void bindViewHolder(FlexibleAdapter adapter, CameraItemHolder holder, int position, List payloads) {
-        holder.title.setText(camera.title.substring(0, 1).toUpperCase() + camera.title.substring(1));
-        holder.location.setText(camera.summary);
+        holder.titleText.setText(camera.title.substring(0, 1).toUpperCase() + camera.title.substring(1));
+        holder.locationText.setText(camera.summary);
 
         if (!isHidden()) {
             holder.cameraView.setCamera(camera);
+            holder.setMapLocation(new LatLng(camera.lat, camera.lng));
         }
     }
 
-    static class CameraItemHolder extends FlexibleViewHolder {
 
-        final TextView title;
-        final TextView location;
+
+    static class CameraItemHolder extends FlexibleViewHolder implements OnMapReadyCallback {
+
+        final TextView titleText;
+        final TextView locationText;
         final CameraView cameraView;
+        final MapView mapView;
+
+        GoogleMap map;
+        LatLng location;
 
         CameraItemHolder(View view, FlexibleAdapter adapter) {
             super(view, adapter);
-            title = (TextView) view.findViewById(R.id.item_camera_title);
-            location = (TextView) view.findViewById(R.id.item_camera_location);
+            titleText = (TextView) view.findViewById(R.id.item_camera_title);
+            locationText = (TextView) view.findViewById(R.id.item_camera_location);
             cameraView = (CameraView) view.findViewById(R.id.item_camera_view);
+            mapView = (MapView) view.findViewById(R.id.item_camera_map);
+            mapView.onCreate(null);
+            mapView.getMapAsync(this);
         }
 
+        public void setMapLocation(LatLng location) {
+            this.location = location;
+            if (map != null) {
+                showPointOnMap();
+            }
+        }
 
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            this.map = googleMap;
+            map.setBuildingsEnabled(false);
+            map.setIndoorEnabled(false);
+            map.setMyLocationEnabled(false);
+            map.getUiSettings().setAllGesturesEnabled(false);
+            map.getUiSettings().setCompassEnabled(false);
+            map.getUiSettings().setIndoorLevelPickerEnabled(false);
+            map.getUiSettings().setMapToolbarEnabled(false);
+            map.setTrafficEnabled(true);
+
+            if (location != null) {
+                showPointOnMap();
+            }
+        }
+
+        private void showPointOnMap() {
+            map.clear();
+            map.addCircle(new CircleOptions()
+                              .center(location)
+                              .fillColor(Color.argb(160, 255, 0, 0))
+                              .strokeColor(Color.RED)
+                              .strokeWidth(1.0f)
+                              .radius(500));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 9f));
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+
+        
     }
 }
